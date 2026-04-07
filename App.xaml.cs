b8f71390484184d0a1224c30cf4835b31541f933
Shubert Chan <1969515540@qq.com>
2026-04-08 01:47:54 +0800

    Refactor App initialization and add notification permissions
    Updated the App class to initialize the MAUI Community Toolkit and request notification permissions on startup.

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Maui.Controls;
-using FutureBound.Page;
+using Microsoft.Maui.Controls;
+using FutureBound.Pages;
+using FutureBound.Data;
+using CommunityToolkit.Maui;
 
 namespace FutureBound
 {
@@ -8,9 +10,12 @@ namespace FutureBound
         public App()
         {
             InitializeComponent();
+            // Initialize the MAUI Community Toolkit
+            MauiProgram.CreateMauiApp();
+            MainPage = new NavigationPage(new FutureBound.Page.SplashPage());
 
-            // 设置启动页
-            MainPage = new NavigationPage(new SplashPage());
+            // Request notification permissions (when the APP starts)
+            _ = NotificationHelper.RequestNotificationPermissionAsync();
         }
     }
 }
